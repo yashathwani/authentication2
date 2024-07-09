dd3defb06b29f3c2201be85bced38fa315b5c343
mrinal1224 <iammrinalkc@gmail.com>
2024-07-09 20:19:52 +0530

    push

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const User = require("../models/userModel");
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+
+
+});
+
+router.post("/login", async (req, res) => {
+  
+});
+
+
+module.exports = router;
