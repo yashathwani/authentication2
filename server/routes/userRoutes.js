76479cf08ce769d60e378d26edab877bce08f749
mrinal1224 <iammrinalkc@gmail.com>
2024-07-10 10:50:24 +0530

    push

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const User = require("../models/userModel");
+const bcrypt = require('bcrypt')
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
 
     try {
+
+        const userExists = await User.findOne({email:req.body.email})
+
+        if(userExists){
+            res.send({
+                success : false,
+                message : "User Already Exists"
+            })
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(req.body.password , salt)
+        req.body.password = hashedPassword
+
+
 
         const newUser = new User(req.body)
         await newUser.save()
