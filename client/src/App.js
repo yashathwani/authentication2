dc87b84ccb433a3880b9cb155e6c49ef08494f83
mrinal1224 <iammrinalkc@gmail.com>
2024-07-12 11:05:58 +0530

    push

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
     
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/" element={<ProtectedRoute/>} />
+          <Route path="/" element={<Home/>} />
          
         </Routes>
       </BrowserRouter>
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -13,7 +13,7 @@ function Login() {
       const response = await LoginUser(values);
       if (response.success) {
         message.success(response.message);
-        localStorage.setItem('token', response.token);
+        // localStorage.setItem('token', response.token);
         navigate('/');
       } else {
         message.error(response.message);
